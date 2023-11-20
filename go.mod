63f1ec0680f37d46f4fa050610845b3336070b08
wirawan-mahardika <wirawanmahardika10@gmail.com>
2023-11-20 20:23:15 +0800

    menambah parameter pada function SayHello

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/wirawan-mahardika/go-say-hello
+module github.com/wirawan-mahardika/go-say-hello/v2
 
 go 1.20
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func SayHello() string {
-	return "Hello world this was updated in 20:02 20/11/2023"
+func SayHello(name string) string {
+	return "hello " + name
 }
